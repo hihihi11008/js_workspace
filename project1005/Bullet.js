e8484b325582116ebb4f6020a26d03919f60fb1f
jieonKIM <hihihi11008@gmail.com>
2020-10-08 09:08:05 +0900

    project1005 커밋

--- project1005/Bullet.js
+++ project1005/Bullet.js
@@ -25,6 +25,24 @@ class Bullet{
     //총알의 물리량 변화 
     tick(){
         this.y+=this.velY;
+
+        //총알이 한걸음, 한걸음 진행할때마다 배열에 존재하는 모든 전국을 대상으로 
+        //충돌했는지 여부를 판단할꺼야
+        for(var i=0;i<enemyArray.length;i++){
+            var result=collisionCheck(this.img, enemyArray[i].img);
+            if(result){
+                // alert("맞췃댱");
+                //너죽고(적군배열에서 요소 삭제 splice(index 삭제), 화면에서도 삭제 removeChild)
+                var img=enemyArray[i].img;
+                wrapper.removeChild(img);
+                enemyArray.splice(0,1);
+                
+                //나죽자(총알배열에서 요소 삭제 splice(index 삭제), 화면애서도 삭제 removeChild)
+                wrapper.removeChild(this.img); //화면에서 제거 
+                var index=bulletArray.indexOf(this); //현재 총알의 인스턴스가 배열의 몇번째 존재하는지 알아냄
+                bulletArray.splice(index,1);
+            }
+        }
     }
     //변화된 물리량을 화면에 표시 (그래픽처리)
     render(){
